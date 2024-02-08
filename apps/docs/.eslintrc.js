9b02d3312af41bb01bfc3553f84c76b56521bcac
Mateusz Hadryś <m.hadrys.95@gmail.com>
2024-02-08 17:46:26 +0100

    fix: Fix eslint mdx

--- apps/docs/.eslintrc.js
+++ apps/docs/.eslintrc.js
@@ -3,11 +3,8 @@ module.exports = {
     tsconfigRootDir: __dirname,
     project: './tsconfig.json',
   },
-  // optional, if you want to lint code blocks at the same time
   settings: {
     'mdx/code-blocks': true,
-    // optional, if you want to disable language mapper, set it to `false`
-    // if you want to override the default language mapper inside, you can provide your own
     'mdx/language-mapper': {},
   },
   extends: ['plugin:mdx/recommended'],
