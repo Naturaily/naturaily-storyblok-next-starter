fb0e1358270f17f667b68963063437f553bedba0
Mateusz Hadryś <mateusz.hadrys@naturaily.com>
2024-12-23 18:39:51 +0100

    feat: Render components

--- packages/storyblok-ui/src/api/api.ts
+++ packages/storyblok-ui/src/api/api.ts
@@ -4,13 +4,41 @@ import { env } from '@natu/env';
 import { isDraftMode } from '@natu/storyblok-preview';
 
 import { componentsMap } from '../components/componentsMap';
+import { SBFooter } from '../components/contentTypes/SBFooter';
+import { SBHeader } from '../components/contentTypes/SBHeader';
+import { SBPage } from '../components/contentTypes/SBPage';
+import { SBColumn } from '../components/elements/SBColumn';
+import { SBContainer } from '../components/elements/SBContainer';
+import { SBCta } from '../components/elements/SBCta';
+import { SBGrid } from '../components/elements/SBGrid';
+import { SBImage } from '../components/elements/SBImage';
+import { SBRichtext } from '../components/elements/SBRichtext';
+import { SBRow } from '../components/elements/SBRow';
+import { SBTable } from '../components/elements/SBTable';
+import { SBThemeModeSwitcher } from '../components/elements/SBThemeModeSwitcher/SBThemeModeSwitcher';
+import { SBTypography } from '../components/elements/SBTypography';
 import { relations } from './relations';
 import { getSdk, SdkFunctionWrapper } from './sdk';
 
 export const getStoryblokApi = storyblokInit({
   accessToken: env.NEXT_PUBLIC_STORYBLOK_PREVIEW_TOKEN,
   use: [apiPlugin],
-  components: componentsMap,
+  components: {
+    cta: SBCta,
+    image: SBImage,
+    richtext: SBRichtext,
+    table: SBTable,
+    container: SBContainer,
+    themeModeSwitcher: SBThemeModeSwitcher,
+    typography: SBTypography,
+    grid: SBGrid,
+    column: SBColumn,
+    row: SBRow,
+    // Content types
+    page: SBPage,
+    footer: SBFooter,
+    header: SBHeader,
+  },
 });
 
 interface GetStoryblokSdkInput {
