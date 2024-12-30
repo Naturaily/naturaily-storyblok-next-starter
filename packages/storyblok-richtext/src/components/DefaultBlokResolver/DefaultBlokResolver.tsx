fd770d82ab0917b83f1e09db380eff36f61396c8
Mateusz Hadryś <mateusz.hadrys@naturaily.com>
2024-12-30 16:50:38 +0100

    fix: Fix import trace

--- packages/storyblok-richtext/src/components/DefaultBlokResolver/DefaultBlokResolver.tsx
+++ packages/storyblok-richtext/src/components/DefaultBlokResolver/DefaultBlokResolver.tsx
@@ -1,12 +1,9 @@
-// TODO: fix this
-// @ts-ignore
-import { StoryblokComponent } from '@storyblok/react/rsc';
+import { StoryblokServerComponent } from '@storyblok/react/rsc';
 
 type ComponentProps = Record<string, unknown> & { _uid: string };
 
 export const DefaultBlokResolver = (component: string, props: ComponentProps) => {
   const blok = { ...props, component };
 
-  // eslint-disable-next-line react/destructuring-assignment
-  return <StoryblokComponent blok={blok} key={props._uid} />;
+  return <StoryblokServerComponent blok={blok} key={props._uid} />;
 };
