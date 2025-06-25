712d9a7985e06d0d8569dbf42bcfe024f1d491cb
Filip Mielczarek <filip.mielczarek@naturaily.com>
2025-06-25 15:19:05 +0200

    use default not-found.tsx

--- apps/web/src/app/not-found.tsx
+++ apps/web/src/app/not-found.tsx
@@ -1,15 +1,25 @@
-import { draftMode } from 'next/headers';
+// import { draftMode } from 'next/headers';
 
-import { getStoryblokSdk } from '@natu/storyblok/api';
-import { StoryblokStory } from '@natu/storyblok/DynamicRender';
+// import { getStoryblokSdk } from '@natu/storyblok/api';
+// import { StoryblokStory } from '@natu/storyblok/DynamicRender';
+
+// const NotFound = async () => {
+//   const { isEnabled } = await draftMode();
+//   const { getConfigNode } = getStoryblokSdk({ draftMode: isEnabled });
+
+//   const { data } = await getConfigNode();
+
+//   return <StoryblokStory story={data?.story?.content?.notFoundPage} />;
+// };
+
+// export default NotFound;
 
 const NotFound = async () => {
-  const { isEnabled } = await draftMode();
-  const { getConfigNode } = getStoryblokSdk({ draftMode: isEnabled });
-
-  const { data } = await getConfigNode();
-
-  return <StoryblokStory story={data?.story?.content?.notFoundPage} />;
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+    </div>
+  );
 };
-
 export default NotFound;
